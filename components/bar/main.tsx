7915a6e9f126077077813066ceb7f87de0d335b2
dxu <dean.xu@asml.com>
2021-05-21 09:23:10 +0800

    echarts bar and relayout

--- components/bar/main.tsx
+++ components/bar/main.tsx
@@ -4,8 +4,15 @@ import {RechartsBar} from "./Recharts";
 import {useEffect, useState} from "react";
 import {getFromLS, saveToLS} from "../util/grid";
 import {ChartJSBar} from "./ChartJS";
+import {EChartsBar} from "./ECharts";
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
+
+const charts = [
+  {name: 'Recharts', Pane: RechartsBar},
+  {name: 'ChartJS', Pane: ChartJSBar},
+  {name: 'ECharts', Pane: EChartsBar}
+]
 
 export const BarDemo = () => {
   const [layouts, setLayouts] = useState<Layouts>({})
@@ -15,19 +22,18 @@ export const BarDemo = () => {
     <ResponsiveGridLayout className={''}
                           compactType={'vertical'}
                           layouts={layouts}
+                          rowHeight={100}
                           draggableHandle={'.drag'}
+                          cols={{lg: 12, md: 12, sm: 6, xs: 2, xxs: 1}}
                           onLayoutChange={(_, ls) => setLayouts(ls)}
     >
-      {[
-        {name: 'Recharts', pane: <RechartsBar/>},
-        {name: 'ChartJS', pane: <ChartJSBar/>},
-      ].map(({name, pane}) => (
-        <div key={name}>
+      {charts.map(({name, Pane}, index) => (
+        <div key={name} data-grid={{w: 6, h: 4, x: (index % 2) * 6, y: index / 2}}>
           <Card className={'w-full h-full relative p-1 flex flex-col'} elevation={2}>
             <Icon icon={'move'} className={'absolute right-2 top-2 cursor-move drag'}/>
             <div className={'text-2xl text-center'}>{name}</div>
-            <div className={'flex-grow w-full relative'}>
-                {pane}
+            <div className={'flex-grow w-full relative overflow-auto'}>
+              <Pane/>
             </div>
           </Card>
         </div>

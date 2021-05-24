7035b8c532de4c6de8655561500e4e46ddf79c7e
dxu <dean.xu@asml.com>
2021-05-24 11:11:51 +0800

    add disable button

--- components/common/Demo.tsx
+++ components/common/Demo.tsx
@@ -1,5 +1,5 @@
 import {Layouts, Responsive, WidthProvider} from 'react-grid-layout';
-import {Card, Divider, Icon} from "@blueprintjs/core";
+import {Card, Divider, Icon, Switch} from "@blueprintjs/core";
 import React, {Profiler, PropsWithChildren, useCallback, useEffect, useState} from "react";
 import {getFromLS, saveToLS} from "../util/grid";
 
@@ -45,15 +45,29 @@ const DemoCard = <T, >({name, Pane, itemProps}: DemoItem<T> & { itemProps: T }) 
   const onRender = useCallback((...args) => {
     console.log(args)
   }, [])
+  const [disable, setDisable] = useState(false)
   return (
     <Card className={'w-full h-full relative p-1 flex flex-col'} elevation={2}>
-      <Icon icon={'move'} className={'absolute right-2 top-2 cursor-move drag-handle'}/>
-      <div className={'text-2xl text-center'}>{name}</div>
+      <Icon icon={'move'}
+            className={'absolute right-2 top-2 cursor-move drag-handle'}/>
+      <Switch checked={disable}
+              onChange={() => setDisable(!disable)}
+              label={'disable'}
+              className={'absolute left-2 top-2'}/>
+      <div className={'text-2xl text-center'}>
+        {name}
+      </div>
       <Divider/>
       <div className={'flex-grow w-full relative overflow-auto'}>
-        <Profiler id={name} onRender={onRender}>
-          <Pane {...itemProps}/>
-        </Profiler>
+        {disable ? (
+          <div className={'w-full h-full flex flex-row items-center justify-center'}>
+            Pane Disabled
+          </div>
+        ) : (
+          <Profiler id={name} onRender={onRender}>
+            <Pane {...itemProps}/>
+          </Profiler>
+        )}
       </div>
     </Card>
   )
